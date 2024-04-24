e1b70f93f9deb393969df5386c775c82cef483e2
ushant <ushant@allianz.lk>
2024-04-24 10:30:20 +0530

    adding and modified react MFE

--- mfe4-react/app.js
+++ mfe4-react/app.js
@@ -1,5 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Link, Outlet, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/mfe4">Home</Link>
+          </li>
+          <li>
+            <Link to="/mfe4/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
 
 class App extends React.Component {
 
@@ -8,15 +45,35 @@ class App extends React.Component {
 
     return (
       <div>
-        <h1>
-          Micro Frontend 4
-          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
-        </h1>
+        <h1>Basic Example</h1>
+  
         <p>
-          React Version: {reactVersion}
+          This example demonstrates some of the core features of React Router
+          including nested <code>&lt;Route&gt;</code>s,{" "}
+          <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
+          "*" route (aka "splat route") to render a "not found" page when someone
+          visits an unrecognized URL.
         </p>
-      </div> 
-    )
+  
+        {/* Routes nest inside one another. Nested route paths build upon
+              parent route paths, and nested route elements render inside
+              parent route elements. See the note about <Outlet> below. */}
+              <BrowserRouter>
+        <Routes>
+          <Route path="/mfe4" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+
+  
+            {/* Using path="*"" means "match anything", so this route
+                  acts like a catch-all for URLs that we don't have explicit
+                  routes for. */}
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
